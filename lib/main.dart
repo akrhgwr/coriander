e3957893a27186175f03ba5a418253043ad0802a
akrhgwr <akr_hgwr@tt.em-net.ne.jp>
2021-09-25 11:04:54 +0900

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -78,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
         //child: RaisedButton(onPressed: () {  },
         child: ElevatedButton(onPressed: (){
           //画面遷移のコード
+          //git 変更テスト
           Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => NextPage()),
