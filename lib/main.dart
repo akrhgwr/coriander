ed91927b92d7b29dfc0f78080a31983f8ebd7832
akrhgwr <akr_hgwr@tt.em-net.ne.jp>
2021-10-03 01:10:27 +0900

    ListView, GridView

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -49,92 +32,129 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  String text = '次へ';
-
-  final myFocusNode = FocusNode();
-
-  String name = '';
-
-  final myController = TextEditingController();
+  final items = List<String>.generate(10000, (i) => "Item $i");
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter大学'),
       ),
       body: Container(
         width: double.infinity,
-        child: Column(
+        /*
+        child: ListView(
           children: <Widget>[
-            TextField(
-              autofocus: true,
-              decoration: InputDecoration(
-                hintText: 'First Name'
-              ),
-              onChanged: (text) {
-                name = text;
-                print('First text field: $text');
-              },
-            ),
-            TextField(
-              controller: myController,
-              focusNode: myFocusNode,
-              decoration: InputDecoration(
-                    hintText: 'Last Name'
-                ),
-              onChanged: (text2) {
-                print('First text field2: $text2');
-              },
-            ),
-            ElevatedButton(
-              //style: raisedButtonStyle,
-              onPressed: ()  {
-                // TODO: ここにフォーカスするためのコード
-                //myFocusNode.requestFocus();
-                print(myController.text);
-                showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        // Retrieve the text the user has entered by using the
-                        // TextEditingController.
-                         content: Text(myController.text),
-                      );
-                    },
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('お問い合わせ'),
+              trailing: Icon(Icons.arrow_forward_ios),
+              onTap: (){
+                // TODO: 画面遷移
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextPage('next page!!')),
                 );
-              },
-              child: Text('サインアップ'),
-            )
-
+              }
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
           ],
         ),
+        */
+        /*
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index]),
+            );
+          },
+        ),
+        */
+        /*
+        child: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            );
+          }),
+        ),
+        */
+        /*
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: List.generate(100, (index) {
+            return Column(
+              children: <Widget>[
+                Expanded(
+                  child: Image.network(
+                        'https://picsum.photos/250?image=9'
+                    ),
+                ),
+                Text('Item $index'),
+              ],
 
+            );
+          }),
+        ),
+        */
+        /* */
+        child: ListView(
+          // This next line does the trick.
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+
+            Column(
+              //width: 160.0,
+              //color: Colors.red,
+              children: <Widget>[
+                Expanded(
+                  child: Image.network(
+                      'https://picsum.photos/250?image=9'
+                  ),
+                ),
+                Text('Item'),
+              ],
+            ),
+
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.blue,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.green,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.yellow,
+            ),
+            Container(
+              width: 160.0,
+              height: 80,
+              color: Colors.orange,
+            ),
+          ],
+        ),
+        /* */
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
